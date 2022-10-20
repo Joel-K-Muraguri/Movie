fe74b5fef6b263b58fc9880c90d06f45af9070e3
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2022-10-20 19:02:57 +0300

    logging interceptor

--- app/src/main/java/com/joel/movie/di/MovieModule.kt
+++ app/src/main/java/com/joel/movie/di/MovieModule.kt
@@ -1,26 +1,42 @@
 package com.joel.movie.di
 
-import com.joel.movie.BuildConfig
 import com.joel.movie.data.network.MovieApiService
-import com.joel.movie.data.repository.MovieRepo
 import com.joel.movie.data.repository.MovieRepository
 import com.joel.movie.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object MovieModule {
 
+//    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+//    logging.setLevel(Level.BASIC);
+//
+//    OkHttpClient client = new OkHttpClient.Builder()
+//    .addInterceptor(logging)
+//    .build();
+
     @Provides
     @Singleton
     fun providesRetrofit(): MovieApiService {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BASIC
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
         return Retrofit.Builder()
+            .client(client)
+
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -30,7 +46,5 @@ object MovieModule {
     @Provides
     @Singleton
     fun providesRepository(apiService: MovieApiService) : MovieRepository =  MovieRepository(apiService)
-
-
 
 }
